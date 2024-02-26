b1f2ab2e6edbeae2fc056c1bfba7125b228d095e
Benjamin Dalsass <benjamin.dalsass@combodo.com>
2024-02-26 10:15:01 +0100

    N°6937 - Symfony 6.4 - Handle Symfony configuration files

--- resources/symfony/config/preload.php
+++ resources/symfony/config/preload.php
@@ -1,9 +1,7 @@
 <?php
 
-use Combodo\iTop\Application\Helper\Session;
-
 // compute app kernel prod container
-$sEnv =  Session::Get('itop_env', 'production');
+$sEnv =  utils::GetCurrentEnvironment();
 $sAppKernelProdContainer = APPROOT . "/data/cache-$sEnv/symfony/App_KernelProdContainer.preload.php";
 
 if (file_exists($sAppKernelProdContainer)) {
